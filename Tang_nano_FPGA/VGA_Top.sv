8c44af781483a5f1670c8c1d68b923f485c9a827
Mohammad Wael <102371006+mwael2002@users.noreply.github.com>
2024-11-21 21:54:51 +0200

    Add files via upload

---
+++ Tang_nano_FPGA/VGA_Top.sv
@@ -0,0 +1,39 @@
+module VGA_TOP #(data_width=3,parameter h_width=10,v_width=10)(
+    input   clk,rst_n,
+    input [data_width-1:0] data_in,
+    output  h_sync,v_sync,
+    output  [data_width-1:0] rgb_data
+);
+
+wire [v_width-1:0]v_count;
+wire [h_width-1:0]h_count;
+
+wire clk_out;
+
+assign rgb_data=(video_on)?data_in:0;
+
+    Gowin_rPLL PLL0(
+        .clkout(clk_out), //output clkout
+        .clkin(clk) //input clkin
+    ); 
+
+counter #(.h_width(h_width),.v_width(v_width)) counter_0(
+.clk(clk),
+.rst_n(rst_n),
+.v_count(v_count),
+.h_count(h_count)
+);
+
+synchronizer #(.h_width(h_width),.v_width(v_width)) synchronizer_0(
+.rst_n(rst_n),
+.clk(clk),
+.v_count(v_count),
+.h_count(h_count),
+.h_sync(h_sync),
+.v_sync(v_sync),
+.video_on(video_on)
+);
+
+
+
+endmodule
--- Tang_nano_FPGA/counter.v
+++ Tang_nano_FPGA/counter.v
@@ -1,1 +1,35 @@
+module counter #(parameter  h_width=10,v_width=10,h_sync_count=640,v_sync_count=480,
+                            right_border=16,left_border=47,top_border=32,bottom_border=10,
+                            h_retrace=96,v_retrace=2)(
+    input  clk,rst_n,
+    output reg[v_width-1:0]v_count,
+    output reg[h_width-1:0]h_count
+);
 
+
+localparam  h_max=h_sync_count+right_border+left_border+h_retrace,v_max=v_sync_count+top_border+bottom_border+v_retrace;
+
+wire v_flag,h_flag;
+
+assign v_flag = (v_count==v_max)?1:0;
+assign h_flag = (h_count==h_max)?1:0;
+
+always @(posedge clk ,negedge rst_n) begin
+    if(!rst_n) begin
+        v_count<=0;
+        h_count<=0;
+    end
+    else if(!h_flag) begin
+        h_count<=h_count+1;
+    end
+    else if(!v_flag)begin
+        v_count<=v_count+1;
+        h_count<=0;
+    end
+    else begin
+        v_count<=0;
+        h_count<=0;
+    end
+end
+
+endmodule
